360f50ac5d11478bf9383622e89e379a01690a1b
Junaira <advshakerullah@gmail.com>
2024-09-28 16:54:47 +0600

    first commit

---
+++ Set operations.py
@@ -0,0 +1,15 @@
+mySet = {1,2,3,3,4,5,5,5}
+print(mySet)
+
+mySet.add("Junaira")
+mySet.add("Kashfia")
+mySet.add("Tabassum")
+print(mySet)
+
+mySet.pop()
+mySet.pop()
+print(mySet)
+
+li = [1,2,3,2]
+setlist = set(li)
+print(setlist)
