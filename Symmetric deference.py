319fe574c6428793dc114ad7dd86c4e6fdabc325
Junaira <advshakerullah@gmail.com>
2024-10-01 17:18:41 +0600

    symmetric

---
+++ Symmetric deference.py
@@ -0,0 +1,19 @@
+#A.
+set1 = {"blue" , "green"}
+set2 = {"blue","yellow"}
+#Original element
+print(f"Original elements = {set1}  and  {set2}")
+
+set3 = set1.symmetric_difference(set2)
+#Symmetric difference
+print(f"Their Symmetric differnce = {set3} " )
+
+#B.
+setA = {1,2,3,4,5}
+setB = {1,5,6,7,8,9}
+#Original element
+print(f"Original elements = {setA}  and  {setB}")
+
+setc = setA.symmetric_difference(setB)
+#Symmetric difference
+print(f"Their Symmetric differnce = {setc} " )
